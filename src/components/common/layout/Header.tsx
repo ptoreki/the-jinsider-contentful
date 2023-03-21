5400097880d4784ee828b6d258e03fe0e4fae554
Patrick Toreki <ptorekigit@gmail.com>
2023-03-21 01:42:02 +0100

    commented out sm links as they did not load proper

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -13,11 +13,11 @@ const Header = () => {
             <li className='header-container__navbar__link'><Link to="/posts" style={{ color: 'inherit', textDecoration: 'inherit', fontFamily: "Roboto, sans-serif"}}>News</Link></li>
             <li className='header-container__navbar__link'><Link to="/podcasts" style={{ color: 'inherit', textDecoration: 'inherit',fontFamily: "Roboto, sans-serif"}}>Podcast</Link></li>
             <li className='header-container__navbar__link'><Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit', fontFamily: "Roboto, sans-serif"}}>About</Link></li>
-            <div style={{marginLeft:1306}}>
+{/*             <div style={{marginLeft:1306}}>
               <a href='https://youtube.com/@ptoreki/' className='header-container__navbar__sm-link'><img src ="src\assets\images\twitter.svg" alt="Twitter link" width={35}/></a>
               <span style={{marginLeft:12, marginRight:12}}></span>
               <a href='https://twitter.com/PatrickToreki' className='header-container__navbar__sm-link'><img src ="src\assets\images\youtube.svg" alt="YouTube link" width={35}/></a>
-            </div>
+            </div> */}
           </ul>
         </div>
       </nav>
