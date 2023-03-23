7245694c3877fe282d25c1227db80f7cd00ba82d
Patrick Toreki <ptorekigit@gmail.com>
2023-03-23 21:56:13 +0100

    fixed sm links

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -14,9 +14,9 @@ const Header = () => {
             <li className='header-container__navbar__link'><Link to="/podcasts" style={{ color: 'inherit', textDecoration: 'inherit',fontFamily: "Roboto, sans-serif"}}>Podcast</Link></li>
             <li className='header-container__navbar__link'><Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit', fontFamily: "Roboto, sans-serif"}}>About</Link></li>
             { <div style={{marginLeft:1306}}>
-              <a href='https://youtube.com/@ptoreki/' className='header-container__navbar__sm-link'><img src ="public\images\youtube.svg" alt="Twitter link" width={30}/></a>
+              <a href='https://youtube.com/@ptoreki/' className='header-container__navbar__sm-link'><img src ="/public/images/twitter.svg" alt="Twitter link" width={30}/></a>
               <span style={{marginLeft:12, marginRight:12}}></span>
-              <a href='https://twitter.com/PatrickToreki' className='header-container__navbar__sm-link'><img src ="public\images\twitter.svg" alt="YouTube link" width={30}/></a>
+              <a href='https://twitter.com/PatrickToreki' className='header-container__navbar__sm-link'><img src ="/public/images/twitter.svg" alt="YouTube link" width={30}/></a>
             </div> }
           </ul>
         </div>
