9646983c39e7a3ca45e47445db604b36f24981eb
Patrick Toreki <ptorekigit@gmail.com>
2024-01-06 12:32:38 +0100

    updated sm links

--- src/components/common/layout/Header.tsx
+++ src/components/common/layout/Header.tsx
@@ -1,29 +1,88 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-    <header className='header-container'>
-      <div className='header-container__title-container'>
-        <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}} className='header-container__title-container__title'><h1 style={{marginTop:0}}>the JINSIDER</h1></Link>
-      </div>
-      <nav>
-        <div className='navbar'>
-          <ul>
-            <li className='header-container__navbar__link'><Link to="/posts" style={{ color: 'inherit', textDecoration: 'inherit', fontFamily: "Roboto, sans-serif"}}>News</Link></li>
-            <li className='header-container__navbar__link'><Link to="/podcasts" style={{ color: 'inherit', textDecoration: 'inherit',fontFamily: "Roboto, sans-serif"}}>Podcast</Link></li>
-            <li className='header-container__navbar__link'><Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit', fontFamily: "Roboto, sans-serif"}}>About</Link></li>
-            { <div style={{marginLeft:1306}}>
-              <a href='https://twitter.com/PatrickToreki' className='header-container__navbar__sm-link'><img src ="images/twitter.svg" alt="Twitter link" width={30}/></a>
-              <span style={{marginLeft:12, marginRight:12}}></span>
-              <a href='https://youtube.com/@ptoreki/' className='header-container__navbar__sm-link'><img src ="images/youtube.svg" alt="YouTube link" width={30}/></a>
-            </div> }
-          </ul>
+      <header className="header-container">
+        <div className="header-container__title-container">
+          <Link
+            to="/"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+            className="header-container__title-container__title"
+          >
+            <h1 style={{ marginTop: 0 }}>the JINSIDER</h1>
+          </Link>
         </div>
-      </nav>
+        <nav>
+          <div className="navbar">
+            <ul>
+              <li className="header-container__navbar__link">
+                <Link
+                  to="/posts"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "inherit",
+                    fontFamily: "Roboto, sans-serif",
+                  }}
+                >
+                  News
+                </Link>
+              </li>
+              <li className="header-container__navbar__link">
+                <Link
+                  to="/podcasts"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "inherit",
+                    fontFamily: "Roboto, sans-serif",
+                  }}
+                >
+                  Podcast
+                </Link>
+              </li>
+              <li className="header-container__navbar__link">
+                <Link
+                  to="/about"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "inherit",
+                    fontFamily: "Roboto, sans-serif",
+                  }}
+                >
+                  About
+                </Link>
+              </li>
+              {
+                <div style={{ marginLeft: 1306 }}>
+                  <a
+                    href="https://twitter.com/HidingJin"
+                    className="header-container__navbar__sm-link"
+                  >
+                    <img
+                      src="images/twitter.svg"
+                      alt="Twitter link"
+                      width={30}
+                    />
+                  </a>
+                  <span style={{ marginLeft: 12, marginRight: 12 }}></span>
+                  <a
+                    href="https://www.youtube.com/channel/UCYHGIDtgzb9GEWsGAHB4NaA"
+                    className="header-container__navbar__sm-link"
+                  >
+                    <img
+                      src="images/youtube.svg"
+                      alt="YouTube link"
+                      width={30}
+                    />
+                  </a>
+                </div>
+              }
+            </ul>
+          </div>
+        </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Header;
--- src/pages/PodcastsPage.tsx
+++ src/pages/PodcastsPage.tsx
@@ -1,8 +1,21 @@
-
 const PodcastsPage = () => {
   return (
-    <div style={{ marginTop: 100, marginBottom: 850, fontSize: 50, fontFamily:"playfair display"}}>Coming soon...</div>
-  )
-}
+    <div
+      style={{
+        marginTop: 100,
+        marginBottom: 850,
+        fontSize: 50,
+        fontFamily: "playfair display",
+      }}
+    >
+      <ul>
+        <li>https://open.spotify.com/show/67Fh4Dr1lO1qrlSnq4yGEK</li>
+        <li>
+          https://www.youtube.com/playlist?list=PLnAu_56QCuvQmeNSrdfvvHDSJpmYJCgxa
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default PodcastsPage
+export default PodcastsPage;
