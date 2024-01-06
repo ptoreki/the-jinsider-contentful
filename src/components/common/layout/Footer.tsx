913896437022ab1e7cc54f179a664c9088c4cc01
Patrick Toreki <ptorekigit@gmail.com>
2024-01-06 12:36:36 +0100

    Update Footer.tsx

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -1,7 +1,5 @@
 const Footer = () => {
-  return (
-      <footer>&#169; 2023 The Jinsider. All rights reserved</footer>
-  )
-}
+  return <footer>&#169; 2024 The Jinsider. All rights reserved</footer>;
+};
 
 export default Footer;
